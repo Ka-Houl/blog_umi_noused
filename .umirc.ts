701e2c603f4890bff982c481e1d6ca27b47723b1
Ka-Houl <646676256@qq.com>
2020-03-05 02:01:14 +0800

    feat:modify config

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,68 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   routes: [
     { path: '/', component: '@/pages/index' },
+    { path: '/list', component: '@/pages/list' },
   ],
+  history: {
+    type: 'hash',
+  },
+  hash: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: { webpackChunkName: true },
+        title: {
+          defaultTitle: '个人博客',
+        },
+        dll: true,
+        locale: {
+          enable: true,
+          // default: 'en-US',
+
+          default: 'zh-CN',
+        },
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//,
+          ],
+        },
+        // chunks: ['vendors', 'umi'],
+        chunks: ['umi'],
+      },
+    ],
+  ],
+  chainWebpack(config, { webpack }) {
+    config.optimization.splitChunks({
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        // vendors: {
+        //   name: 'vendors',
+        //   chunks: 'all',
+        //   test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom|lodash|lodash-decorators|redux-saga|re-select|dva|moment)[\\/]/,
+        //   priority: -10,
+        // },
+        umi: {
+          name: 'umi',
+          chunks: 'all',
+          test: /[\\/]node_modules[\\/](@ant-design|antd)[\\/]/,
+          priority: -11,
+        },
+      },
+    });
+  },
 });
--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { history } from 'umi';
-import styles from './index.less';
 import { Button } from 'antd';
 
 const a :number= 1
@@ -8,12 +7,11 @@ const a :number= 1
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 >Page list222</h1>
       <Button type="primary" onClick={() =>{
-        history.push('/list');
-      }}>Primary</Button>212um2121212i plugin list11122
+      }}>Primary</Button>umi plugin list11122
       <div className='test'> 1111111</div>
-      <div className={styles.test}> 111哇哦113311</div>
+      <div > 111113311</div>
     </div>
   );
 };
