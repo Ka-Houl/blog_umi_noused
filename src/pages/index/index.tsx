a413f017ddfa61139995156ebcca2c2af740d957
Ka-Houl <646676256@qq.com>
2020-03-06 18:28:47 +0800

    feat:new commit

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,16 +3,22 @@ import { history } from 'umi';
 import styles from './index.less';
 import { Button } from 'antd';
 
-const a :number= 1
+const a: number = 1;
 
 export default () => {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
-      <Button type="primary" onClick={() =>{
-        history.push('/list');
-      }}>Primary</Button>212um2121212i plugin list11122
-      <div className='test'> 1111111</div>
+      <Button
+        type="primary"
+        onClick={() => {
+          history.push('/list');
+        }}
+      >
+        Primary
+      </Button>
+      212um2121212i plugin list11122
+      <div className="test"> 1111111</div>
       <div className={styles.test}> 111哇哦113311</div>
     </div>
   );
--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { history } from 'umi';
 import { Button } from 'antd';
 
-const a :number= 1
+const a: number = 1;
 
 export default () => {
   return (
     <div>
-      <h1 >Page list222</h1>
-      <Button type="primary" onClick={() =>{
-      }}>Primary</Button>umi plugin list11122
-      <div className='test'> 1111111</div>
-      <div > 111113311</div>
+      <h1>Page list222</h1>
+      <Button type="primary" onClick={() => {}}>
+        Primary
+      </Button>
+      list
+      <div className="test"> 1111121212111111111111111111</div>
+      <div> 111113311</div>
     </div>
   );
 };
