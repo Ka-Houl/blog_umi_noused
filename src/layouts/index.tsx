80d927e8771023d2e00dbaebf9d7d82e9c3ac449
Ka-Houl <646676256@qq.com>
2020-03-06 13:10:23 +0800

    feat: modify menu

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,32 +1,37 @@
-import React from 'react'
-import {Menu,Layout } from 'antd'
+import React from 'react';
+import { Menu, Layout } from 'antd';
 import { history } from 'umi';
 
 const { Header, Footer, Content } = Layout;
-export default (props:any) =>{
-  console.log(props)  
-    return (
-      <>
-        <Layout>
-          <Header>
-            <Menu
-              theme="light"
-              mode="horizontal"
-              // defaultSelectedKeys={['2']}
-              selectedKeys={[props.location.pathname]}
-              style={{ lineHeight: '64px' }}
-              onClick={(item,) =>{
-                history.push(item.key);
-              }}
-            >
-              <Menu.Item key="/">主页</Menu.Item>
-              <Menu.Item key="/list">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
-            </Menu>
-          </Header>
-          <Content> { props.children }</Content>
-          <Footer>Footer</Footer>
-        </Layout>
-      </>
-    );
-  }
+export default (props: any) => {
+  console.log(props);
+  const menu = [
+    { key: '/', name: '主页' },
+    { key: '/list', name: '列表' },
+    { key: '/github', name: 'github' },
+  ];
+  return (
+    <>
+      <Layout>
+        <Header>
+          <Menu
+            theme="light"
+            mode="horizontal"
+            // defaultSelectedKeys={['2']}
+            selectedKeys={[props.location.pathname]}
+            style={{ lineHeight: '64px' }}
+            onClick={item => {
+              history.push(item.key);
+            }}
+          >
+            {menu.map(item => (
+              <Menu.Item key={item.key}>{item.name}</Menu.Item>
+            ))}
+          </Menu>
+        </Header>
+        <Content> {props.children}</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </>
+  );
+};
