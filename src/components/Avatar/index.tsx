ffac905a1db8422ccb6856572e43b3e71b95415a
Ka-Houl <646676256@qq.com>
2020-03-08 03:17:51 +0800

    fix:修复tooltip 下myicon报错

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -13,20 +13,31 @@ export default props => {
       <div className={styles.work}>前端开发工程师</div>
       <Divider>社交帐号</Divider>
       <div className={styles.icon_wrapper}>
-        <Tooltip placement="top" title="Email:kahoul@foxmail.com">
-          <MyIcon type="icon-youxiang" className={styles.icon} />
+        <Tooltip placement="top" title="kahoul@foxmail.com">
+          {/* 加一层div嵌套，防止myicon没有tooltip相关事件引用的问题 */}
+          <div>
+            <MyIcon type="icon-youxiang" className={styles.icon} />
+          </div>
         </Tooltip>
-        {/* <Tooltip placement="top" title="QQ:646676256">
-          <MyIcon type="icon-qq" className={styles.icon} />
-        </Tooltip> */}
+        <Tooltip placement="top" title="QQ:646676256">
+          <div>
+            <MyIcon type="icon-qq" className={styles.icon} />
+          </div>
+        </Tooltip>
         <Tooltip placement="top" title="wechat:Ka-houl">
-          <MyIcon type="icon-weixin-copy" className={styles.icon} />
+          <div>
+            <MyIcon type="icon-weixin-copy" className={styles.icon} />
+          </div>
         </Tooltip>
         <Tooltip placement="top" title="https://github.com/Ka-Houl">
-          <MyIcon type="icon-github" className={styles.icon} />
+          <div>
+            <MyIcon type="icon-github" className={styles.icon} />
+          </div>
         </Tooltip>
         <Tooltip placement="top" title="18883993447">
-          <MyIcon type="icon-phone" className={styles.icon} />
+          <div>
+            <MyIcon type="icon-phone" className={styles.icon} />
+          </div>
         </Tooltip>
       </div>
     </div>
